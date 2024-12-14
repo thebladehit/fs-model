0ccd51453607dd378fc5a9c1420a58dceaa77ce1
thebladehit <boqdanyarmolka@gmail.com>
2024-12-15 01:09:57 +0200

    Add examples of usage

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,56 @@ import config from './config/config';
 const logger = new Logger();
 const os = new OS(logger, config);
 
-os.mkfs(5);
+// examples for fifth lab
+os.mkfs(15);
+os.mkdir('dir1');
+os.stat('dir1');
+os.mkdir('dir1/dir2');
+os.stat('dir1');
 os.ls();
+os.stat('/');
+os.cd('dir1/dir2');
+os.create('file.txt');
+os.mkdir('/a');
+os.mkdir('/a/b');
+os.symlink('/dir1', '/a/b/l1');
+os.ls('/a/b');
+os.open('/a/b/l1/dir2/file.txt');
+os.cd('..');
+os.pwd();
+os.symlink('dir2/./../../dir1/././dir2/file.txt', '/dir1/l2');
+os.cd('/a/b');
+os.open('l1/l2');
+os.cd('/');
+os.unlink('/dir1/l2');
+os.unlink('/a/b/l1');
+os.unlink('dir1/dir2/file.txt');
+os.unlink('dir1/dir2');
+os.symlink('some', 'dir1/dir2/data.txt');
+os.ls('dir1/dir2');
+os.link('dir1/dir2/data.txt', '/a/b/document');
+os.ls('/a/b');
+os.stat('/a/b/document');
+os.stat('/dir1/dir2/data.txt');
+os.open('dir1/dir2/data.txt');
+os.create('some');
+os.open('/dir1/dir2/data.txt');
+os.mkdir('/1');
+os.mkdir('/1/2');
+os.cd('/1/2');
+os.rmdir('../2');
+os.ls();
+
+// examples for fourth lab
+// os.pwd();
+// os.cd('/abc/abc/sym1');
+// os.pwd();
+// os.create('/abc/text.txt');
+// os.ls();
+// os.create('/abc/aa/text.txt');
+// os.stat('text.txt')
+// os.pwd();
+// os.stat('./')
 
 // the lowes fd index example
 // os.create('text.txt');
@@ -53,10 +101,20 @@ os.ls();
 // os.read(2, 3);
 
 // example of many links to one file
-os.create('text.txt');
-os.link('text.txt', 'newtext.txt');
-os.ls();
+// os.create('text.txt');
+// os.link('text.txt', 'newtext.txt');
+// os.ls();
 
+
+// os.create('file.txt');
+// os.open('file.txt');
+// os.truncate('file.txt', 1024);
+// os.seek(0, 512);
+// os.write(0, 3, '512');
+// os.seek(0, 1024);
+// os.write(0, 4, '1024');
+// os.seek(0, 0);
+// os.read(0, 2048);
 
 // os.link('text.txt', 'newtext.txt');
 // os.ls();
